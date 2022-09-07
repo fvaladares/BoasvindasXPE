2f5b310a6a7e722e42cb03b674ecd17e506d60c2
Fabricio Valadares <fabricio.valadares@gmail.com>
2022-09-06 22:24:43 -0300

    Implementação da passagem de parâmetros utilizando intents entre duas atitivies.
    Retorno dos dados utilizando o startactivityforresult;
    
    Utilização do material edit text para exibir mensagem de erro caso o campo esteja vazio.

--- app/src/main/java/br/com/xpeducacao/boasxpe/MainActivity.kt
+++ app/src/main/java/br/com/xpeducacao/boasxpe/MainActivity.kt
@@ -2,6 +2,8 @@ package br.com.xpeducacao.boasxpe
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import br.com.xpeducacao.boasxpe.databinding.ActivityMainBinding
 import br.com.xpeducacao.boasxpe.telaresultado.BoasVindasActivity
@@ -9,14 +11,26 @@ import br.com.xpeducacao.boasxpe.telaresultado.BoasVindasActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val retorno = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { activityResult ->
+        activityResult.data?.let { intent ->
+            if (intent.hasExtra(TAG_RETORNO)) {
+                val retorno = intent.getStringExtra(TAG_RETORNO)
+                binding.etNome.setText(retorno)
+            } else {
+                Log.d(
+                    "XPE::Info",
+                    "Não foram retorandos dados da activity chamada"
+                )
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
-
         prepararListeners()
     }
 
@@ -25,15 +39,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun configurarBtnEnviarListener() {
-        binding.button.setOnClickListener {
+        binding.btnEnviar.setOnClickListener {
+            enviarDados()
+        }
+    }
+
+    private fun enviarDados() {
+        val nome = binding.etNome.text
+        if (nome.isNullOrBlank()) {
+            binding.tilNome.error = "Este campo deve ser obrigatoriamente preenchido."
+        } else {
+            binding.tilNome.error = null
             val intent = Intent(this, BoasVindasActivity::class.java)
-            val nome = binding.etNome.text.toString()
-            intent.putExtra(TAG_NOME, nome)
-            startActivity(intent)
+            intent.putExtra(TAG_NOME, nome.toString())
+            retorno.launch(intent)
         }
     }
 
     companion object {
         const val TAG_NOME = "tag_nome"
+        const val TAG_RETORNO = "tag_retorno"
     }
 }
--- app/src/main/java/br/com/xpeducacao/boasxpe/telaresultado/BoasVindasActivity.kt
+++ app/src/main/java/br/com/xpeducacao/boasxpe/telaresultado/BoasVindasActivity.kt
@@ -1,6 +1,8 @@
 package br.com.xpeducacao.boasxpe.telaresultado
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import br.com.xpeducacao.boasxpe.MainActivity
 import br.com.xpeducacao.boasxpe.R
@@ -13,13 +15,27 @@ class BoasVindasActivity : AppCompatActivity() {
         binding = ActivityBoasVindasBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        configurarBtnRetornar()
         receberDadosEnviados()
+    }
+
+    private fun configurarBtnRetornar() {
+
+        binding.btnRetornar.setOnClickListener {
+            val retorno = binding.etRetorno.text.toString()
+            Intent().apply {
+                putExtra(MainActivity.TAG_RETORNO, retorno)
+                setResult(RESULT_OK, this)
+            }
+            finish()
+        }
     }
 
     private fun receberDadosEnviados() {
         val nome = intent.getStringExtra(MainActivity.TAG_NOME)
+        Log.d("XPE::Debug", nome.toString())
         binding.apply {
-            tvNome.text = getString(R.string.lbl_boas_vindas_nome, nome)
+            tvBoasVindaslbl.text = getString(R.string.lbl_boas_vindas_nome, nome)
         }
     }
 }
